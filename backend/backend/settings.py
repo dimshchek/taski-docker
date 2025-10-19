2778c52fc7f787e93840ed4bed216c4169248e18
Dmitry Shcheklein <dim.shchek@yandex.ru>
2025-10-19 19:10:21 +0300

    123

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['chaplashka.hopto.org', '89.169.169.139', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['89.169.169.139', '127.0.0.1', 'localhost', 'chaplashka.hopto.org']
 
 
 # Application definition
